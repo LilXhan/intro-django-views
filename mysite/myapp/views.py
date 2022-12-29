740f7fc2e2a99847aa201e13d6f58dc8b55ea63f
Xhan <alvaradotuctod@gmail.com>
2022-12-29 18:11:36 -0500

    second class

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Person
 
-
+# siempre las funciones reciben un request
 def index(request):
     return HttpResponse('Hello World, from Django')
+
+
+def index_two(request):
+    people = Person.objects.all()
+
+    context = {
+        'people': people
+    }
+
+    return render(request, 'index.html', context)
